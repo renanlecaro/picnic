c13e1afa05b8581cb7a6174a2038e71688e5ed3a
Renan LE CARO <renan.lecaro@gmail.com>
2022-01-07 01:26:29 +0100

    First very basic version

---
+++ client.js
@@ -0,0 +1,122 @@
+const editor=document.getElementById('editor')
+const id=location.pathname.slice(1)
+function crash(message){
+  alert(message)
+}
+
+function bufferToS(buffer){
+ return  btoa(String.fromCharCode.apply(null, new Uint8Array(buffer)))
+}
+function sToBuffer(base64_string){
+  return Uint8Array.from(atob(base64_string), c => c.charCodeAt(0)).buffer
+}
+
+
+async function getKey(){
+
+  if(location.hash){
+
+
+    const key=await crypto.subtle.importKey(
+      'raw',
+      sToBuffer(location.hash.slice(1)),
+      'AES-GCM',
+      true,
+
+      ["encrypt", "decrypt"]
+    );
+return  key
+  }else{
+
+    const key=await crypto.subtle.generateKey( {
+        name: "AES-GCM",
+        length: 256
+      },
+      true,
+      ["encrypt", "decrypt"]);
+
+    const exported = await window.crypto.subtle.exportKey(
+      "raw",
+      key
+    );
+
+    location.hash=bufferToS(exported)
+    return key
+  }
+}
+
+
+getKey()
+  .then((key)=>{
+
+    const socket = new WebSocket('ws://'+location.host);
+    function send( data){
+      socket.send(JSON.stringify( data))
+    }
+
+    function updateTitle(decoded){
+
+      document.title=(decoded.split('\n').find(l=>l.trim()) || 'New document').slice(0,50)
+    }
+
+    async function setText(parsed){
+      const decryptedButBinary=await window.crypto.subtle.decrypt(
+        {
+          name: "AES-GCM",
+          iv: sToBuffer(parsed.iv)
+        },
+        key,
+        sToBuffer(parsed.ciphertext)
+      );
+
+      const decoded=new TextDecoder().decode(decryptedButBinary)
+      editor.innerText=decoded
+      updateTitle(decoded)
+      editor.style.backgroundColor='hsl('+Math.floor(Math.random()*360)+', 100%, 90%)'
+    }
+    if(startText){
+      setText(startText)
+    }
+
+    socket.addEventListener('open', function (event) {
+      send({action:'join-room', id})
+
+      socket.addEventListener('message', function (event) {
+        try{
+
+          const parsed=JSON.parse(event.data)
+          switch (parsed.action){
+            case 'set-text':
+              setText(parsed)
+              break
+          }
+        }catch (e){
+          crash(e)
+        }
+      });
+
+      editor.addEventListener('keyup',async e=>{
+        try{
+          const decoded=e.target.innerText
+          updateTitle(decoded)
+
+          const iv = await window.crypto.getRandomValues(new Uint8Array(12));
+          const encrypted= await window.crypto.subtle.encrypt(
+            {
+              name: "AES-GCM",
+              iv: iv
+            },
+            key,
+            new TextEncoder().encode(decoded)
+          );
+          send({action:'set-text', ciphertext:bufferToS(encrypted), iv:bufferToS(iv), id})
+        }catch(e){
+          console.error(e)
+        }
+      })
+
+    });
+
+  }).catch(e=>{
+  crash(e.message || 'Unknown error')
+})
---
+++ server.js
@@ -0,0 +1,83 @@
+const express = require('express')
+const Handlebars = require("handlebars");
+const crypto=require('crypto')
+const { WebSocketServer } =require('ws');
+
+const app = express()
+const port = 4444
+
+const fs=require('fs')
+
+
+const clientHTML = Handlebars.compile(fs.readFileSync('./client.hbs').toString());
+
+
+const clientJS=fs.readFileSync('./client.js').toString()
+
+app.get('/', (req, res) => {
+  console.log('redirecting')
+  res.redirect('/'+randomId())
+})
+app.get('/:id',  async (req, res) => {
+
+  const startText=JSON.stringify(await getText(req.params.id));
+  res.setHeader('Content-Type', 'text/html');
+  res.end(clientHTML({clientJS,startText}))
+})
+
+const server=app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+function randomId(){
+  return crypto.randomBytes(8).toString("hex")
+}
+
+function idToFilePath(id){
+  return './data/'+id.toString().replace(/[^a-z0-9]/gi,'')+'.dat.json'
+}
+
+async function getText(id){
+  const path=idToFilePath(id)
+
+  if(!fs.existsSync(path)){
+    return null
+  }
+  return  JSON.parse(fs.readFileSync(path).toString())
+}
+
+async function setText(id, text){
+  return fs.writeFileSync(idToFilePath(id), JSON.stringify(text))
+}
+
+
+const rooms={}
+const wss = new WebSocketServer({ server });
+wss.on('connection', function connection(ws) {
+
+  ws.on('message', async function message(data) {
+    const parsed = JSON.parse(data)
+    console.log(parsed)
+    switch (parsed.action){
+      case 'join-room':
+        ws.roomId=parsed.id
+        if(!rooms[parsed.id]){
+          rooms[parsed.id]=[]
+        }
+        rooms[parsed.id].push(ws)
+        break;
+      case 'set-text':
+        (rooms[parsed.id]||[]).forEach(wst=>wst !== ws && wst.send(JSON.stringify(parsed)))
+        await setText(parsed.id, parsed)
+        break;
+    }
+  });
+  ws.on('close', ()=>{
+    const id=ws.roomId
+    if(id){
+      rooms[id]=(rooms[id]||[]).filter(wst=>wst!==ws)
+      if(rooms[id].length) delete rooms[id]
+    }
+  })
+});
+
