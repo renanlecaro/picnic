b6b0bde0b7cd10b69f5735cdd8214d785c77722f
Renan LE CARO <renan.lecaro@gmail.com>
2022-01-11 12:43:25 +0100

    Click the status box to move it

--- src/client/updateInfoToast.js
+++ src/client/updateInfoToast.js
@@ -1,4 +1,15 @@
 const info = document.getElementById("info");
+
+let infoBoxPosition = parseInt(localStorage.getItem("infoBoxPosition") || "0");
+function switchInfoMode() {
+  infoBoxPosition = (infoBoxPosition + 1) % 4;
+
+  localStorage.setItem("infoBoxPosition", infoBoxPosition.toString());
+  info.setAttribute("data-mode", infoBoxPosition.toString());
+}
+info.setAttribute("data-mode", infoBoxPosition.toString());
+info.addEventListener("click", switchInfoMode);
+
 export function updateInfoToast(text) {
   info.className = text ? "show" : "";
   info.innerText = text;
